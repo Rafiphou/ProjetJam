f6e77cbabd8d1483a95993ee37abd1cf621691f4
Lags-33 <charlie.pommier@icloud.com>
2026-07-15 18:19:51 +0200

    Defense Point en surbrillance quand survolés par la souris

---
+++ CastleCrab/Assets/Scripts/DefensePoint.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class DefensePoint : MonoBehaviour
+{
+    public GameObject defensePointBase;
+    public GameObject defensePointActive;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        defensePointBase.SetActive(true);
+        defensePointActive.SetActive(false);
+    }
+
+    private void OnMouseOver()
+    {
+        defensePointBase.SetActive(false);
+        defensePointActive.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        defensePointBase.SetActive(true);
+        defensePointActive.SetActive(false);
+    }
+}
