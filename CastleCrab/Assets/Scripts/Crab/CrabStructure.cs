83a55e28bda3799491217472c524cc7ccc5a1daf
Tymmi78 <timothee.gallier@outlook.fr>
2026-07-16 15:38:41 +0200

    feat(crab) : added move behaviour

--- CastleCrab/Assets/Scripts/Crab/CrabStructure.cs
+++ CastleCrab/Assets/Scripts/Crab/CrabStructure.cs
@@ -1,111 +1,214 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 
 
-namespace Crab
+public class CrabStructure : MonoBehaviour
 {
-    public class CrabStructure
+    protected int max_rss_carried_;
+    protected List<RessourceType> rss_carried;
+    protected int walking_speed_;
+    protected int attack_;
+    protected float attack_speed_;
+    protected float health_;
+    protected float area_vision_;
+
+    protected (int, int) coordinates_;
+    protected (int, int) coordinates_target_;
+
+    protected bool is_triggered_;
+
+    protected bool is_near_castle_;
+    
+    protected Transform target_;
+
+    public RessourcePoint actual_point;
+
+
+    public CrabStructure(int max_rss, int walking_speed, int attack, float attack_speed, float health,
+        float area_vision, (int, int) coordinates)
     {
-        protected int max_rss_carried_;
-        protected List<RessourceType> rss_carried;
-        protected int walking_speed_;
-        protected int attack_;
-        protected float attack_speed_;
-        protected float health_;
-        protected float area_vision_;
+        max_rss_carried_ = max_rss;
+        rss_carried = new List<RessourceType>();
+        walking_speed_ = walking_speed;
+        attack_ = attack;
+        attack_speed_ = attack_speed;
+        health_ = health;
+        area_vision_ = area_vision;
+        coordinates_ = coordinates;
+        coordinates_target_ = (0, 0);
+        actual_point = null;
+        is_triggered_ = false;
+        target_ = null;
+        is_near_castle_ = true;
+    }
 
-        protected (int, int) coordinates_;
-        protected (int, int) coordinates_target_;
+    public List<RessourceType> getRssCarried()
+    {
+        return rss_carried;
+    }
 
-        protected bool is_triggered_;
-        
-        public RessourcePoint actual_point;
-        
+    public void setMaxRss(int new_max)
+    {
+        max_rss_carried_ = new_max;
+    }
 
-        public CrabStructure(int max_rss, int walking_speed, int attack, float attack_speed, float health,
-            float area_vision, (int, int) coordinates)
+    public int getWalkingSpeed()
+    {
+        return walking_speed_;
+    }
+
+    public void setWalkingSpeed(int new_speed)
+    {
+        walking_speed_ = new_speed;
+    }
+
+    public int getAttack()
+    {
+        return attack_;
+    }
+
+    public void setAttack(int new_attack)
+    {
+        attack_ = new_attack;
+    }
+
+    public float getAttackSpeed()
+    {
+        return attack_speed_;
+    }
+
+    public void setAttackSpeed(float new_attack_speed)
+    {
+        attack_speed_ = new_attack_speed;
+    }
+
+    public float getHealth()
+    {
+        return health_;
+    }
+
+    public void setHealth(float new_health)
+    {
+        health_ = new_health;
+    }
+
+    public void takeDamage(int damage)
+    {
+        health_ -= damage;
+        if (health_ < 0)
         {
-            max_rss_carried_ = max_rss;
-            rss_carried = new List<RessourceType>();
-            walking_speed_ = walking_speed;
-            attack_ = attack;
-            attack_speed_ = attack_speed;
-            health_ = health;
-            area_vision_ = area_vision;
-            coordinates_ = coordinates;
-            coordinates_target_ = (0,0);
-            actual_point = null;
-            is_triggered_ = false;
+            health_ = 0;
         }
+    }
 
-        public List<RessourceType> getRssCarried() { return rss_carried; }
-        
-        public void setMaxRss(int new_max) { max_rss_carried_ = new_max; }
+    public float getAreaVision()
+    {
+        return area_vision_;
+    }
 
-        public int getWalkingSpeed() { return walking_speed_; }
-        public void setWalkingSpeed(int new_speed) { walking_speed_ = new_speed; }
+    public void setMaxAreaVision(float new_max)
+    {
+        area_vision_ = new_max;
+    }
 
-        public int getAttack() { return attack_; }
-        public void setAttack(int new_attack) { attack_ = new_attack; }
+    public (int, int) getCoordinates()
+    {
+        return coordinates_;
+    }
 
-        public float getAttackSpeed() { return attack_speed_; }
-        public void setAttackSpeed(float new_attack_speed) { attack_speed_ = new_attack_speed; }
+    public void setCoordinates(int x, int y)
+    {
+        coordinates_ = (x, y);
+    }
 
-        public float getHealth() { return health_; }
-        public void setHealth(float new_health) { health_ = new_health; }
+    public (int, int) getCoordinatesTarget()
+    {
+        return coordinates_target_;
+    }
 
-        public void takeDamage(int damage)
+    public void setCoordinatesTarget(int x, int y)
+    {
+        coordinates_target_ = (x, y);
+    }
+    
+    public Transform getTarget() { return target_; }
+    public void setTarget(Transform new_target){ target_ = new_target; }
+
+    public void addRessource()
+    {
+        for (int i = 0; i < max_rss_carried_; i++)
         {
-            health_ -= damage;
-            if (health_ < 0)
+            List<RessourceType> rss_gathered = actual_point.GetRandomRessource();
+
+            rss_carried.Add(rss_gathered[0]);
+            if (rss_gathered.Count > 1)
             {
-                health_ = 0;
+                rss_carried.Add(rss_gathered[1]);
             }
         }
+    }
 
-        public float getAreaVision() { return area_vision_; }
-        public void setMaxAreaVision(float new_max) { area_vision_ = new_max; }
+    public void changeActualPoint(RessourcePoint new_actual_point)
+    {
+        actual_point = new_actual_point;
+    }
 
-        public (int, int) getCoordinates() { return coordinates_; }
-        public void setCoordinates(int x, int y) { coordinates_ = (x, y); }
-        
-        public (int, int) getCoordinatesTarget() { return coordinates_target_; }
-        public void setCoordinatesTarget(int x, int y) { coordinates_target_ = (x, y); }
-        
-        public void addRessource()
+    public void moveToTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target_.position, walking_speed_ * Time.deltaTime);
+        if (Math.Abs(target_.position.x - transform.position.x) < 0.1 && Math.Abs(target_.position.y - transform.position.y) < 0.1)
         {
-            for (int i = 0; i < max_rss_carried_; i++)
+            if (!is_near_castle_)
             {
-                List<RessourceType> rss_gathered = actual_point.GetRandomRessource();
-                
-                rss_carried.Add(rss_gathered[0]);
-                if (rss_gathered.Count > 1)
-                {
-                    rss_carried.Add(rss_gathered[1]);
-                }
+                //target = castle
             }
+            else
+            {
+                target_ = null;
+            }
+            
+        }
+    }
+
+    public void trigger()
+    {
+        is_triggered_ = true;
+    }
+
+    public void giveRessources()
+    {
+        //castle.addRessouce(rss_carried)
+        rss_carried.Clear();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Castle")
+        {
+            is_near_castle_ = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Castle")
+        {
+            is_near_castle_ = false;
+        }
+    }
+
+    public void Update()
+    {
+        if (is_triggered_)
+        {
+            //wait next target
         }
 
-        public void changeActualPoint(RessourcePoint new_actual_point)
+        if (target_ != null)
         {
-            actual_point = new_actual_point;
-        }
-
-        public void moveToTarget(Transform target)
-        {
-            //do the movement behavior
-            //Vector3.MoveTowards( ... , target.position, walking_speed_ * Time.deltaTime); //?
-        }
-
-        public void trigger()
-        {
-            is_triggered_ = true;
-        }
-
-        public void selectTarget()
-        {
-            //need to retrieve the clic of the user, to assign a target
+            moveToTarget();
         }
     }
 }
